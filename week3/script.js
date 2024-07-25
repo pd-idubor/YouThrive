3abbc66ab9a9ba60f1536afe8fa540c3ebbb1a33
pd-idubor <preciousidubor@gmail.com>
2024-07-25 12:49:35 +0100

    week3/

---
+++ week3/script.js
@@ -0,0 +1,29 @@
+let firstName = "Ben";
+let lastName = "James";
+let city = "San Francisco"
+let country = "United States"
+let level = "Senior"
+
+let age = 15;
+let height = 145;
+let weight = 35;
+let addressNum = 21;
+let amount = 25000;
+
+let black = false;
+let short = false;
+let rich = true;
+let friend = true;
+let family = true;
+
+let work = null;
+let partner = null;
+let car = null;
+let company = null;
+let airline = null;
+
+let college = undefined;
+let model = undefined;
+let children = undefined;
+let skill = undefined;
+let habit = undefined;
