62fd3fcdc16f87fd692eb15c7c427ce84fa84575
Precious Idubor <preciousidubor@gmail.com>
2024-09-14 05:50:24 +0100

    Error corr

--- RecipePI/server.js
+++ RecipePI/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = require('./router');
-const dbClient = require('./database);
+const dbClient = require('./database');
 
 
 const app = express();
@@ -15,5 +15,5 @@ app.use(express.json());
 app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${PORT}`);
-}
+  console.log(`Server is running on port ${port}`);
+});
